0835f912363b3d28747d3eb837255df05938bcc0
Marcus Edwards <edwardsmarc968@gmail.com>
2023-07-21 10:04:11 -0700

    Init repo
    - demo subset of qcircuit parsing

---
+++ qasm_builder.py
@@ -0,0 +1,164 @@
+class Builder:
+    """
+    A class that represents a quantum circuit.
+    """
+
+    def __init__(self, num_qubits=1, qasm=None):
+        """
+        Initializes a quantum state with the given parameters.
+
+        :param num_qubits: The total number of qubits.
+        :param qasm: Predefined qasm provided to initialization.
+        :raises: ValueError
+        """
+        self.num_qubits = num_qubits
+
+        if qasm is None:
+            self.qasm = f'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg {self.symbol}[{num_qubits}];\ncreg c[{num_qubits}];'
+        else:
+            self.qasm = qasm
+
+        self.print()
+
+    def barrier(self, qubit=None):
+        """
+        Applies a barrier to the circuit to disable circuit optimization.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        if qubit:
+            print("barrier ({0})".format(qubit), end='')
+            self.qasm = self.qasm + f'\nbarrier {self.symbol}[{qubit}];'
+        else:
+            print("barrier ({0})".format(self.symbol), end='')
+            self.qasm = self.qasm + f'\nbarrier {self.symbol};'
+        return self
+
+    def x(self, qubit):
+        """
+        Performs a Pauli X gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("x ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\nx {self.symbol}[{qubit}];'
+        return self
+
+    def y(self, qubit):
+        """
+        Performs a Pauli Y gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("y ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\ny {self.symbol}[{qubit}];'
+        return self
+
+    def z(self, qubit):
+        """
+        Performs a Pauli Z gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("z ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\nz {self.symbol}[{qubit}];'
+        return self
+
+    def u1(self, lamb, qubit):
+        """
+        A single parameter single qubit phase gate with zero duration:
+
+        [[1,0],[0,exp(1i*lamb)]]
+
+        :param lamb: The phase parameter.
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("u1({1}) ({0})".format(qubit, lamb), end='')
+        self.qasm = self.qasm + f'\nu1({lamb}) {self.symbol}[{qubit}];'
+        return self
+
+    def u3(self, theta, phi, lamb, qubit):
+        """
+        A three parameter single qubit gate:
+
+        [[cos(theta/2),-exp(1i*lambda)*sin(theta/2)],[exp(1i*phi)*sin(theta/2),exp(1i*lambda+1i*phi)*cos(theta/2)]]
+
+        :param theta: The first parameter.
+        :param phi: The second parameter.
+        :param lamb: The thrid parameter.
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("u3({1}, {2}, {3}) ({0})".format(qubit, theta, phi, lamb), end='')
+        self.qasm = self.qasm + f'\nu3({theta},{phi},{lamb}) {self.symbol}[{qubit}];'
+        return self
+
+    def s(self, qubit):
+        """
+        Performs an S phase shift gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("s ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\ns {self.symbol}[{qubit}];'
+        return self
+
+    def sdg(self, qubit):
+        """
+        Performs an S dagger phase shift gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("sdg ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\nsdg {self.symbol}[{qubit}];'
+        return self
+
+    def cx(self, source, target):
+        """
+        Performs a Controlled X gate on the target qubit with the
+        source qubit as controller.
+
+        :param source: The source qubit.
+        :param source: The target qubit.
+        :return: The full qasm after the opeation.
+        """
+        print("cx ({0} -> {1})".format(source, target), end='')
+        self.qasm = self.qasm + f'\ncx {self.symbol}[{source}], {self.symbol}[{target}];'
+        return self
+
+    def h(self, qubit):
+        """
+        Performs a Hadamard gate on the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The full qasm after the operation.
+        """
+        print("h ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\nh {self.symbol}[{qubit}];'
+        return self
+
+    def m(self, qubit):
+        """
+        Measures the target qubit.
+
+        :param qubit: The target qubit.
+        :return: The result of the measurement.
+        """
+
+        print("m ({0})".format(qubit), end='')
+        self.qasm = self.qasm + f'\nmeasure {self.symbol}[{qubit}] -> c[{qubit}];'
+        return self
+
+    def print(self):
+        """
+        Prints the full qasm.
+        """
+        print("\nIBMQX QASM:")
+        print(self.qasm)
---
+++ qcircuit_parse.py
@@ -0,0 +1,73 @@
+import pdfquery
+from lxml import etree
+from operator import itemgetter
+
+from qasm_builder import Builder
+
+
+GATES = [
+    'x',
+    'y',
+    'z',
+    'u1',
+    'u2',
+    'u3',
+    's',
+    'sdg',
+    'h',
+    'tdg',
+    'cx',
+    'cy',
+    'cz',
+    't',
+    'ccx',
+    'reset',
+    'cu1',
+    'ccy',
+    'ccz'
+]
+
+pdf = pdfquery.PDFQuery("Circuits.pdf")
+pdf.load()
+
+pdf.tree.write('readable.xml', pretty_print=True)
+
+tree = etree.parse(open("readable.xml", "r"))
+root = tree.getroot()
+
+wires = {}
+
+for element in root.iter(tag=etree.Element):
+    if 'x0' in element.attrib.keys():
+        print("%s - %s - (%s, %s)" % (element.tag, element.text, element.attrib['x0'], element.attrib['y0']))
+
+    # if we have a wire
+    if element.tag == 'LTTextLineHorizontal':
+        # get the vertical position
+        y = element.attrib['y0']
+        # register the wire
+        if y not in wires.keys():
+            wires[y] = []
+
+    # if we have a gate
+    if element.tag == 'LTTextBoxHorizontal':
+        # find the wire
+        wire = element.attrib['y0']
+        # register the wire if needed
+        if wire not in wires.keys():
+            wires[wire] = []
+        # register the gate
+        if element.text.strip().lower() in GATES:
+            wires[wire].append({'name': element.text.strip().lower(), 'index': element.attrib['x0']})
+            # sort the wire's gates by x index
+            wires[wire] = sorted(wires[wire], key=itemgetter('index'))
+
+qasm = ''
+
+builder = Builder()
+
+for wire, gates in wires.items():
+    for gate in gates:
+        getattr(builder, gate['name'])(list(wires.keys()).index(wire))
+
+print(builder.qasm)
