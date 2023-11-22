7a0a548262f7a2e33fc109db0cf02264da0e4ab1
Marcus Edwards <edwardsmarc968@gmail.com>
2023-11-22 12:04:22 -0800

    Tool to retrun QASM for images

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A repository for our work on parsing quantum circuit diagrams.
 
 Our data looks like combinations of images (circuits) and text (OpenQASM code). There are
-three utilities for working with these provided in this repo. The first is `qcircuit_parse.py`. If 
+four utilities for working with these provided in this repo. The first is `qcircuit_parse.py`. If 
 you already have a circuit as a .pdf and want to know the OpenQASM that goes along with it, you
 can provide the path to the .pdf to `qcircuit_parse.parse_circuit` and it will return the QASM as 
 a string to you.
@@ -14,6 +14,30 @@ of the quantum gates in the supported list (see `qcircuit_parse.GATES`) arranged
 these parameters and with nearest-neighbour connectivity. Then, you can run pdflatex to turn these into
 .pdfs. Finally, the utility will generate images (.jpgs) from the .pdfs that result.
 
-The third and final tool is `image_classification.py` which accepts the image dataset in the examples
+The third tool is `image_classification.py` which accepts the image dataset in the examples
 folder as an input and runs a basic image classification algorithm on it. Our goal is to improve this
 algorithm!
+
+Finally, there is a trained model that will get your circuit classification right about 80% of the time
+under saved_models. You can use this model to convert provided circuits to QASM like so:
+
+```
+$ python tool.py --input_file path/to/circuit.jpg
+```
+
+This will spit out the (hopefully) correct QASM right in the command line.
+
+```
+Opening the provided image... 
+
+Classifying the quantum circuit... 
+ 
+Converting to QASM: 
+ 
+OPENQASM 2.0;
+include "qelib1.inc";
+
+h q[0];
+x q[1];
+cx q[0], q[1];
+```
--- tool.py
+++ tool.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-import PIL
+import numpy as np
 import getopt
 import sys
+import os
 
 
 class ModelNotFoundException(BaseException):
@@ -10,13 +11,29 @@ class ModelNotFoundException(BaseException):
 
 def get_saved_model():
     try:
-        model = tf.saved_model.load(f'saved_models/trained_model')
+        # get file dir
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        # load model
+        model = tf.saved_model.load(f'{dir_path}/saved_models/trained_model')
         return model
     except OSError as e:
         raise ModelNotFoundException(str(e))
 
 
-HELP_STRING = "Usage: python tool.py -i /path/to/circuit.jpg"
+def preprocess_image(image_path, image_size=180):
+    # read an image file
+    image = tf.io.read_file(image_path)
+    # turn image into numerical tensors with RGBs
+    image = tf.image.decode_jpeg(image, channels=3)
+    # convert color values from 0-255 to 0-1 #Normaliizations
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    # resize image
+    image = tf.image.resize(image, size=[image_size, image_size])
+
+    return image[None, :, :]
+
+
+HELP_STRING = "Usage: python tool.py --input_file /path/to/circuit.jpg"
 
 
 def main(argv):
@@ -33,20 +50,25 @@ def main(argv):
         sys.exit(2)
 
     # comment out this block to test
-    # for opt, arg in opts:
-    #     if opt in ["-h", "--help"]:
-    #         print(HELP_STRING)
-    #         sys.exit()
-    #     elif opt in ["-i", "--input_files"]:
-    #         input_file = arg
+    for opt, arg in opts:
+        if opt in ["-h", "--help"]:
+            print(HELP_STRING)
+            sys.exit()
+        elif opt in ["-i", "--input_files"]:
+            input_file = arg
 
     if input_file is not None:
         print('\x1b[34m Opening the provided image... \n \x1b[37m')
-        img = PIL.Image.open(input_file)
+        img = preprocess_image(input_file)
         print('\x1b[34m Loading the circuit identification model... \n \x1b[37m')
         model = get_saved_model()
         print('\x1b[34m Classifying the quantum circuit... \n \x1b[37m')
         predictions = model(img)
+        print('\x1b[34m Converting to QASM: \n \x1b[37m')
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        qasm = open(f'{dir_path}/examples/gen/circuit_{list(np.array(predictions)[0]).index(min(np.array(predictions)[0]))}.qasm')
+        print(qasm.read())
+        qasm.close()
     else:
         print("The input file is a required parameter.")
 
